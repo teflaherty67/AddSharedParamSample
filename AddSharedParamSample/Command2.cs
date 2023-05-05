d0f3d15266003a666f4be166fe97fe554c824d59
Tom E Flaherty <teflaherty@live.com>
2023-05-05 11:42:25 -0500

    family update code

--- AddSharedParamSample/Command2.cs
+++ AddSharedParamSample/Command2.cs
@@ -25,8 +25,15 @@ namespace AddSharedParamSample
             Document doc = uiapp.ActiveUIDocument.Document;
 
             // Your code goes here
+            SampleFamilyLoadOptions familyLoadOptions = new SampleFamilyLoadOptions();
+            Family myFamily = null;
+            string famPath = @"C:\Temp\EL-Wall Base.rfa";
 
-
+            Transaction t = new Transaction(doc);
+            t.Start("Load Family");
+            doc.LoadFamily(famPath, familyLoadOptions, out myFamily);
+            t.Commit();
+            t.Dispose();
             return Result.Succeeded;
         }
 
@@ -34,6 +41,46 @@ namespace AddSharedParamSample
         {
             var method = MethodBase.GetCurrentMethod().DeclaringType?.FullName;
             return method;
+        }
+    }
+    public class SampleFamilyLoadOptions : IFamilyLoadOptions
+    {
+        public bool OnFamilyFound(bool familyInUse, out bool overwriteParameterValues)
+        {
+            if (!familyInUse)
+            {
+                //TaskDialog.Show("SampleFamilyLoadOptions", "The family has not been in use and will keep loading.");
+
+                overwriteParameterValues = true;
+                return true;
+            }
+            else
+            {
+                //TaskDialog.Show("SampleFamilyLoadOptions", "The family has been in use but will still be loaded with existing parameters overwritten.");
+
+                overwriteParameterValues = true;
+                return true;
+            }
+        }
+
+        public bool OnSharedFamilyFound(Family sharedFamily, bool familyInUse, out FamilySource source, out bool overwriteParameterValues)
+        {
+            if (!familyInUse)
+            {
+                //TaskDialog.Show("SampleFamilyLoadOptions", "The shared family has not been in use and will keep loading.");
+
+                source = FamilySource.Family;
+                overwriteParameterValues = true;
+                return true;
+            }
+            else
+            {
+                //TaskDialog.Show("SampleFamilyLoadOptions", "The shared family has been in use but will still be loaded from the FamilySource with existing parameters overwritten.");
+
+                source = FamilySource.Family;
+                overwriteParameterValues = true;
+                return true;
+            }
         }
     }
 }
